c7ad0087ac6db8c1d365788904b9ac7c3062a48e
Salva Amoros <salvacorreo@hotmail.com>
2018-09-05 02:38:39 +0100

    This is a test file

--- Convert to binary.py
+++ Convert to binary.py
@@ -1,26 +1,47 @@
+import tkinter as tk
 '''Challenge: Convert any number to binary without using bin()'''
 
-def dec_to_bin(number:int) -> bin:
-    '''inputs an integer and returns its binary value'''
+window = tk.Tk()
+window.title("Decimal 2 Binary calculator")
 
+def dec_to_bin(event):
+    '''inputs an integer and returns its binary value'''
+    number = int(decimal.get())
     binary = ''
 
     while number > 0:           # number must be a positive integer.
         binary += str(number % 2)   # calculates the binary number
         number = number // 2
 
-    print(binary[::-1])
+    result.config(text=binary[::-1])
+    # print(binary[::-1])
 
-while True:
+tk.Label(window, text="Introduce a positive integer to convert to binary.").grid(row=0, column=1, columnspan=2)
+tk.Label(window, text="Decimal: ").grid(row=1, column=0)
+decimal = tk.Entry(window)
+tk.Label(window, text="to binary: ").grid(row=1, column=2)
+result = tk.Message(window)
+decimal.grid(row=1, column=1)
+result.grid(row=1, column=3)
 
-    number = input("Introduce a number to convert to binary ('q' to quit):\n")
+button = tk.Button(window, text="Convert")
+button.grid(row=2, column=1, columnspan=2)
+button.focus_force()
+button.bind('<Button-1>', dec_to_bin)
 
-    if number == "q":
-        break       # Finishes the program.
-    else:
-        try:        # If number is not an integer, prints a warning and asks again
-            dec_to_bin(int(number))
-        except ValueError:
-            print("Please, introduce a positive integer to convert to binary.\n")
 
-print("Thanks for using the 'Decimal to Binary' converter")
+# while True:
+#
+#     number = input("Introduce a number to convert to binary ('q' to quit):\n")
+
+# if number == "q":
+#     break       # Finishes the program.
+#         try:        # If number is not an integer, prints a warning and asks again
+#             dec_to_bin(int(number))
+#         except ValueError:
+#             print("Please, introduce a positive integer to convert to binary.\n")
+    # else:
+
+# print("Thanks for using the 'Decimal to Binary' converter")
+
+tk.mainloop()
